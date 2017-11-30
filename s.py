3353204402b6d9db0d3da2ed984e176d8ad78e35
tracerXU <xyh32@126.com>
2017-11-30 12:23:33 +0800

    7

--- s.py
+++ s.py
@@ -19,9 +19,10 @@ def handle_client(sock,addr): #Ò»¸ö×Óº¯Êý£¬×÷Îª»Øµ÷º¯Êý£¬ÔÚÖ÷º¯ÊýÔËÐÐµÄÏß³ÌÖÐ±»µ
 	
 	if data=='1':
 		wiringpi.wiringPiSetupGpio()
-		while True:
-			wiringpi.pinMode(24,1)  
-			wiringpi.digitalWrite(24,1)
+		wiringpi.pinMode(24,1)  
+		wiringpi.digitalWrite(24,1)
+		wiringpi.pinMode(25,1)  
+		wiringpi.digitalWrite(25,0)
 
 		
 while True:
